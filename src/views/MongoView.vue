3325be9e091a8b23243f3e77e0bfaac71d1be95b
Robert3721 <robert.rudnicki11@gmail.com>
2023-10-11 22:45:03 +0200

    added scanner for products

--- src/views/MongoView.vue
+++ src/views/MongoView.vue
@@ -1,8 +1,12 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { v4 as uuid } from "uuid";
+import {StreamBarcodeReader} from "vue-barcode-reader";
 
 const name = ref("");
+const scannerOpened= ref();
+const barcode = ref("");
+const productScanned: boolean = ref(false);
 
 function addToDB() {
   const item = {
@@ -12,12 +16,26 @@ function addToDB() {
   name.value = "";
 }
 
+const onDecode = (text) => {
+  productScanned.value = true;
+  scannerOpened.value = false;
+  barcode.value = text;
+};
+
+function toggleCamera(){
+  scannerOpened.value = !scannerOpened.value;
+}
 
 </script>
 
 
 <template>
   <main>
+     <div>
+     <button @click="toggleCamera">{{scannerOpened ? 'Close scanner'  : 'Add file by scanner'}}</button>
+     <p v-if="productScanned"> Product barcode is : {{barcode}}</p>
+      <StreamBarcodeReader v-if="scannerOpened" @decode="onDecode" @error="onError"></StreamBarcodeReader>
+    </div>
     <div>
       <input v-model="name" class="me-2" type="text">
       <button @click="addToDB">Add item to database</button>
