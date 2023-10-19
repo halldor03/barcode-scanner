8cf9c10e04f578edac8d64e8b11f35a2555905dd
halldor03 <kacoleker@gmail.com>
2023-10-19 21:58:34 +0200

    change start url in manifest.json  and env setting

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ import { InjectManifest } from "workbox-webpack-plugin";
 
 export const config = {};
 
-if (import.meta.env.NODE_ENV !== "production") {
+if (import.meta.env.NODE_ENV === "production") {
   config["configureWebpack"] = { plugins: [new InjectManifest({ swSrc: "./service-worker.js" })] };
 }
